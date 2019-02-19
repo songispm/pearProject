5569e4641efd6f98ac034cf049275125b3971f69
宋寒冬 <songispm@gmail.com>
2019-02-19 13:10:21 +0800

    编译

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 export default {
-    crossDomain: true, //是否开启跨域支持
+    crossDomain: false, //是否开启跨域支持
     PROD_URL: 'https://api.thispm.com/index.php', //生产环境接口地址
     WS_URI: 'wss://www.thispm.com:2345',//WebSocket地址
     HOME_PAGE: '/home',//主页路由
