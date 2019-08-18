3062ec5458d80aabfa4ddfb1a43ff476ea2e39f2
宋寒冬 <songispm@gmail.com>
2019-08-18 16:08:16 +0800

    2.8.4 编译

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ function resolve(dir) {
 
 const HOST = '127.0.0.1';
 const PORT = '8045';
-const DEV_URL = 'https://api.thispm.com/index.php';
+const DEV_URL = process.env.NODE_ENV === 'production' ? 'https://api.thispm.com' : 'http://127.0.0.1/pearProjectApi';
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
     productionSourceMap: false,
